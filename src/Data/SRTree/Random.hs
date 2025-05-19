9a59de1466ade5296ead683054006652c98b147d
folivetti <fabricio.olivetti@gmail.com>
2025-05-19 06:58:08 -0300

    integrate search functions for SR

--- src/Data/SRTree/Random.hs
+++ src/Data/SRTree/Random.hs
@@ -25,6 +25,7 @@ module Data.SRTree.Random
          , randomFunction
          , randomNode
          , randomNonTerminal
+         , randomRange
          , randomTreeTemplate
          , randomTree
          , randomTreeBalanced
