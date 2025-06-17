04d41b28e193d078c608006871d3a28215ea8079
Fabricio Olivetti de Franca <folivetti@ufabc.edu.br>
2025-06-17 17:52:37 -0300

    Update Print.hs

--- src/Data/SRTree/Print.hs
+++ src/Data/SRTree/Print.hs
@@ -22,6 +22,7 @@ module Data.SRTree.Print
          , showPython
          , printPython
          , showLatex
+         , showLatexWithVars
          , printLatex
          , showOp
          )
@@ -153,7 +154,22 @@ showLatex = cata alg . removeProtection
       Uni Abs t     -> concat ["\\left |", t, "\\right |"]
       Uni Recip t   -> concat ["\\frac{1}{", t, "}"]
       Uni f t       -> concat [showLatexFun f, "(", t, ")"]
-
+      
+showLatexWithVars :: [String] -> Fix SRTree -> String
+showLatexWithVars varnames = cata alg . removeProtection
+  where 
+    alg = \case
+      Var ix        -> concat ["\\operatorname{", varnames !! ix, "}"]
+      Param ix      -> concat ["\\theta_{", show ix, "}"]
+      Const c       -> show c
+      Bin Power l r -> concat [l, "^{", r, "}"]
+      Bin Mul l r    -> concat ["\\left(", l, " \\cdot ", r, "\\right)"]
+      Bin Div l r    -> concat ["\\frac{", l, "}{", r, "}"]
+      Bin op l r    -> concat ["\\left(", l, " ", showOp op, " ", r, "\\right)"]
+      Uni Abs t     -> concat ["\\left |", t, "\\right |"]
+      Uni Recip t   -> concat ["\\frac{1}{", t, "}"]
+      Uni f t       -> concat [showLatexFun f, "(", t, ")"]
+                
 showLatexFun :: Function -> String
 showLatexFun f = mconcat ["\\operatorname{", map toLower $ show f, "}"]
 {-# INLINE showLatexFun #-}
